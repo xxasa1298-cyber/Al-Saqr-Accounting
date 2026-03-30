6b70ecc9a85e5b382570d93862ab2a830db774f1
xxasa1298-cyber <xxasa1298@gmail.com>
2026-03-30 19:09:47 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+﻿from flask import Flask
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+@app.route("/")
+def home():
+    return "Backend is running successfully!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
