be4144bd0f0a013e01e8ef58dd22e382c0db0357
xxasa1298-cyber <xxasa1298@gmail.com>
2026-04-01 01:07:06 +0300

    Create UserControlDashboard.Designer.cs

---
+++ UserControlDashboard.Designer.cs
@@ -0,0 +1,8 @@
+partial class UserControlDashboard
+{
+    private void InitializeComponent()
+    {
+        this.BackColor = Color.FromArgb(60, 60, 60);
+        this.Dock = DockStyle.Fill;
+    }
+}
